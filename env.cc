1b54a1e144a2163f68285ef7d65984a0edfc7866
yuanrongxi <747669766@qq.com>
2014-09-08 22:19:42 +0800

    modify leveldb

--- env.cc
+++ env.cc
@@ -16,12 +16,13 @@ void Log(Logger* info_log, const char* format, ...)
 static Status DoWriteStringToFile(Env* env, const Slice& data, const std::string& fname, bool should_sync)
 {
 	WritableFile* file;
+	//获得一个文件写对象
 	Status s = env->NewWritableFile(fname, &file);
 	if(!s.ok())
 		return s;
-
+	//进行文件追加
 	s = file->Append(data);
-	if(s.ok() && should_sync){
+	if(s.ok() && should_sync){ //同步写入硬盘
 		s = file->Sync();
 	}
 
@@ -30,7 +31,7 @@ static Status DoWriteStringToFile(Env* env, const Slice& data, const std::string
 	}
 
 	delete file;
-	if(!s.ok()){
+	if(!s.ok()){ //写失败，直接删除文件？
 		env->DeleteFile(fname);
 	}
 
@@ -49,8 +50,39 @@ Status WriteStringToFileSync(Env* env, const Slice& data, const std::string& fna
 
 Status ReadFileToString(Env* env, const std::string& fname, std::string* data)
 {
+	data->clear();
 
+	SequentialFile* file;
+	//获得一个文件读对象
+	Status s = env->NewSequentialFile(fname, &file);
+	if(!s.ok()){
+		return s;
+	}
+
+	//每次读取数据块大小
+	static const int kBufferSize = 8192;
+	char* space = new char[kBufferSize];
+	//进行数据读取
+	while(true){
+		Slice fragment;
+		s = file->Read(kBufferSize, &fragment, space);
+		if(!s.ok()){ //读取失败
+			break;
+		}
+		//拼接数据
+		data->append(fragment.data(), fragment.size());
+		if(fragment.empty()){
+			break;
+		}
+	}
+
+	delete []space;
+	delete file;
+
+	return s;
 }
+
+
 
 }
 
--- posix_logger.h
+++ posix_logger.h
@@ -7,7 +7,81 @@
 #include <time.h>
 #include "env.h"
 
+namespace leveldb{
+//POSIX模式下的日志实现
+class PosixLogger : public Logger
+{
+public:
+	PosixLogger(FILE* f, uint64_t (*gettid)()) : file_(f), gettid_(gettid){};
+	virtual ~PosixLogger()
+	{
+		fclose(file_);
+	};
 
+	virtual void Logv(const char* format, va_list ap)
+	{
+		const uint64_t thread_id = (*gettid_)();
+		char buffer[500];
+		for(int iter = 0; iter < 2; iter ++){
+			char* base;
+			int bufsize;
+			if(iter == 0){
+				bufsize = sizeof(buffer);
+				base = buffer;
+			}
+			else{
+				bufsize = 30000;
+				base = new char[bufsize];
+			}
+
+			char* p = base;
+			char* limit = base + bufsize;
+
+			struct timeval now_tv;
+			gettimeofday(&now_tv, NULL);
+			const time_t seconds = now_tv.tv_sec;
+			struct tm t;
+			localtime_r(&seconds, &t);
+			//获得日志时间
+			p += snprintf(p, limit - p,
+                    "%04d/%02d/%02d-%02d:%02d:%02d.%06d %llx ",
+                    t.tm_year + 1900,
+                    t.tm_mon + 1,
+                    t.tm_mday,
+                    t.tm_hour,
+                    t.tm_min,
+                    t.tm_sec,
+                    static_cast<int>(now_tv.tv_usec),
+                    static_cast<long long unsigned int>(thread_id));
+
+			if(p < limit){
+				if(iter == 0){ //数据没有完全拼接完成，开辟一个更大的缓冲区
+					continue;
+				}
+				else {
+					p = limit - 1;
+				}
+			}
+
+			//判断换行
+			if(p == base || p[-1] != '\n'){
+				*p++ = '\n';
+			}
+			//文件写入
+			assert(p < limit);
+			fwrite(base, 1, p - base, file_);
+			fflush(file_);
+			if(base != buffer){ //重新开辟了新的缓冲区来做日志写入
+				delete []base;
+			}
+			break;
+		}
+	}
+
+private:
+	FILE* file_;
+	uint64_t (*gettid_)();
+};
 
 #endif
 
